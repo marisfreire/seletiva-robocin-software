3683f5f757ef703558e3b19eb13c017ec9a0462a
marisfreire <marisousafreire@gmail.com>
2025-01-15 17:03:43 -0300

    Expandi a minha ideia para solucionar a Fase 1

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from utils.ssl.Navigation import Navigation
 from utils.ssl.base_agent import BaseAgent
+from agent_decision import shortest_path
 
 class ExampleAgent(BaseAgent):
     def __init__(self, id=0, yellow=False):
@@ -16,11 +17,24 @@ class ExampleAgent(BaseAgent):
         # como Navigation.goToPoint() é um método estático, não posso alterá-lo
         # sendo assim eu penso que eu tenho que fazer o robô navegar até pontos "parciais" do caminho até o target real, mas já conhecendo o caminho ideal inteiro 
         # os pontos parciais vão ser definidos de tal forma: se houver obstáculos no caminho, o ponto parcial será um pouco antes de cada obstáculo, após isso ele deve DESVIAR do obstáculo e continuar até o target/próximo ponto parcial.
-        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
+        # de um modo, isso já nos dá o caminho ideal pois sabemos que a menor distância entre dois pontos é uma reta, que é o que goToPoint() tenta fazer. Nos resta agora fazer com que o agente desvie dos obstáculos do seu caminho. (PARA O MODO EASY)
+        obstacles = {**self.opponents, **self.teammates}
+        partial_points = shortest_path(obstacles, self.robot.x, self.robot.y, self.targets[0].x, self.targets[0].y)
+        partial_points.append(self.targets[0])
+        
+        # PROBLEMA: Não ainda consigo fazer com que o agente vá até um ponto e DEPOIS vá até outro
+        
+        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, partial_points[0])
+        partial_points = partial_points[1:]
         self.set_vel(target_velocity)
         self.set_angle_vel(target_angle_velocity)
-
+            
+        
         return
 
+        
+
     def post_decision(self):
+        # o processo de pós decision vai ser relevante quando possuirmos mais de um target e, portanto, mais de um agente a segui-lo.
+        # após o agente chegar ao seu target, ele deverá decidir se fica parado ou se há outro target disponível para ele
         pass
--- agent_decision.py
+++ agent_decision.py
@@ -1,5 +1,27 @@
+import math
 # tenho um agente que se move e um target designado a ele
 # tenho a posição do target e tenho a posição do robô
 # também tenho a posição dos seus obstáculos
-# posso testar caminhos até o alvo em que tenham o menor número de (X,Y) iguais aos (X,Y) de algum dos obstáculos
+# vou desviar dos obstaculos cujos (X,Y) interceptam o caminho do agente até o target
 
+
+def intersection(coef_ang, coef_linear, op):
+    crossed = False
+    # Checar se a reta intercepta alguma parte do círculo do agente
+    return crossed
+
+def shortest_path(obstacles, pos_agent_x, pos_agent_y, pos_target_x, pos_target_y):
+    partial_points = []
+
+    # Temos dois pontos conhecidos, então podemos usar a equação da reta
+    # Estou usando uma reta pois a navegação é com base na distância euclidiana entre os pontos
+    # PROBLEMA: preciso levar em consideração o fato de que os obstáculos tem um certo raio, ou seja, podem haver partes suas que sejam interceptadas pela reta e não necessariamente seu centro.
+    coef_ang = int((pos_agent_y - pos_target_y)/(pos_agent_x - pos_target_x))
+    coef_linear = int(pos_agent_y - (coef_ang * pos_agent_x))
+
+
+    for op in obstacles:
+        if (intersection(coef_ang, coef_linear, op)):
+            partial_points.append(op)
+
+    return partial_points
